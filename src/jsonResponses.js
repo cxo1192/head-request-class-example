8869ab92768d34009bd88d2565bbd819541dcadd
Cameron Ostrom (RIT Student) <cxo1192@ad.rit.edu>
2019-09-13 14:50:17 -0400

    head request demo

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -2,32 +2,54 @@
 const users = {};
 
 const respondJSON = (request, response, status, object) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
 
+  response.writeHead(status, headers);
+  response.write(JSON.stringify(object));
+  response.end();
 };
 
 const respondJSONMeta = (request, response, status) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
 
+  response.writeHead(status, headers);// /same thing as respondJSON but only headers
+  response.end();
 };
 
 const getUsers = (request, response) => {
+  const responseJSON = {
+    users,
+  };
 
+  return respondJSON(request, response, 200, responseJSON);
 };
 
-const getUsersMeta = (request, response) => {
-
-};
+const getUsersMeta = (request, response) => respondJSONMeta(request, response, 200);
 
 const updateUser = (request, response) => {
+  const newUser = {
+    createdAt: Date.now(),
 
+  };
+
+  users[newUser.createdAt] = newUser;
+  return respondJSON(request, response, 201, newUser);
 };
 
 const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found!',
+    id: 'notFound',
+  };
 
+  return respondJSON(request, response, 404, responseJSON);
 };
 
-const notFoundMeta = (request, response) => {
-
-};
+const notFoundMeta = (request, response) => respondJSONMeta(request, response, 404);
 
 module.exports = {
   getUsers,
